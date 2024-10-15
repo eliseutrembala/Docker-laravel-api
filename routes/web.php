c911600d7a805843952b3b9d408e70a3b2fe6d61
eliseutrembala <104913722+eliseutrembala@users.noreply.github.com>
2024-10-15 09:23:55 -0300

    mudanças

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/somar', function (Request  $request) {
+    if (count($request->all()) < 1) {
+        return response()->json([
+            'message' => 'Não há valores para somar.'
+        ], 406);
+    }
+    $soma = array_sum($request->all());
+    return response()->json([
+        'message' => 'somado com sucesso',
+        'sum' => $soma,
+    ]);
 });
