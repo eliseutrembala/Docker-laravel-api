830043678c106b4cb3cca7ada2ff0cf056755717
eliseutrembala <eliseuc715@gmail.com>
2024-10-22 08:52:09 -0300

    aula dia 22 docker
    criando uma nova migration

---
+++ app/Models/People.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class People extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 
+        'name',
+        'email',
+        'address',
+        'state',
+        'country',
+        'phone',
+        'birthDate',
+    ];
+}
